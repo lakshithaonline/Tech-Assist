e4068d74f2e6383d50f427b0b3df67f886c30ca8
Lakshitha Geethmal <lakshithageethmal@gmail.com>
2021-09-07 01:25:19 +0530

    php

--- Contact Us/function.php
+++ Contact Us/function.php
@@ -1,4 +1,11 @@
 <?php
+class Contact{
+    public $host="eu-cdbr-west-01.cleardb.com";
+    public $user="b30408a4390f55";
+    public $pass="9f19047b";
+    public $db="heroku_bd0c0b070629615";
+    public $mysqli;
 
+}
 
 ?>
