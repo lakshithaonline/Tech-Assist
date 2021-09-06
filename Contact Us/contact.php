4cb401567272db64f6c4ecac200eca8a4c622369
Lakshitha Geethmal <lakshithageethmal@gmail.com>
2021-09-07 01:19:30 +0530

    Update contact.php

--- Contact Us/contact.php
+++ Contact Us/contact.php
@@ -12,7 +12,13 @@
 
                 <div class="col-lg-8 col-lg-offset-2">
 
+                 <h1>Contact Tech Assist</h1>
 
+                 <p class="lead">While we're good with smoke signals, there are simpler ways for us to get in touch and answer your questions</p>
+
+                 <form id="contact-form" method="post" role="form">
+                     
+                 </form>
 
                 </div>
             </div>
