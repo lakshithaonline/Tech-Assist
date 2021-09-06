8e503dfad125ce5cb05624f4463212f12dc8e713
Lakshitha Geethmal <lakshithageethmal@gmail.com>
2021-09-07 01:16:42 +0530

    contact page html + css + php

--- Contact Us/contact.php
+++ Contact Us/contact.php
@@ -1,1 +1,11 @@
-
+<html>
+    <head>
+        <title>Contact Us</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='style.css' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        
+    </body>
+</html>
