5e0ec1eb1edc8f68bf5e71f41edbf57eef3f0cb7
Lakshitha Geethmal <lakshithageethmal@gmail.com>
2021-09-07 01:24:35 +0530

    css update

--- Contact Us/contact.php
+++ Contact Us/contact.php
@@ -3,6 +3,8 @@
         <title>Contact Us</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
         <link href='style.css' rel='stylesheet' type='text/css'>
     </head>
     <body>
